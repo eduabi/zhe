2013a62b08047315db99e4de2692f651680cea77
wuyangyang <wuyangyang9104@zto.com>
2020-10-16 17:15:00 +0800

    初始化

---
+++ index.ts
@@ -0,0 +1,3 @@
+import { Earth } from './src/observer'
+let _earth: Earth = new Earth()
+document.body.innerHTML = _earth.getName();
---
+++ src/observer.ts
@@ -0,0 +1,11 @@
+class Earth {
+  name: string = 'earth';
+  getName() {
+    return this.name
+  }
+}
+
+let human: string = 'Human being lives on Earth.';
+
+export { Earth, human };
+
